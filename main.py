ce174a9b8193576fcc18f968d87d2c1c683cba7d
Han <puhoi125@mil.ru>
2024-10-02 14:37:12 +0300

    02/10

---
+++ main.py
@@ -0,0 +1,9 @@
+my_dict = {'lavra': 12457890, 'troi':22366745, 'nastia': 2143567809}
+print(my_dict)
+print(my_dict['troi'])
+my_dict['nastia'] = 33456633289
+my_dict['anton'] = 895477643321
+print(my_dict)
+my_dict.update({'max': 33442211 ,'lola':45890889068})
+del my_dict['lavra']
+print(my_dict)
